22fe8e6b3684443e3e4296d7ff0d0d16a0b4b605
Aaron <ajhotimfat@gmail.com>
2025-04-13 19:18:41 -0400

    Added the ClientManager class.

---
+++ src/main/java/org/example/programmingpatternsproject/ClientManager.java
@@ -0,0 +1,29 @@
+package org.example.programmingpatternsproject;
+
+import java.util.HashMap;
+
+public class ClientManager {
+    private static HashMap<String, Client> clients;
+
+    private ClientManager() {
+
+    }
+
+    public static HashMap<String, Client> getClients() {
+        if (clients == null) {
+            clients = new HashMap<>();
+        }
+        return clients;
+    }
+
+    public void addClient(Client client) {
+        clients.put(client.getUserId(), client);
+    }
+
+    public Client searchClientById(String userId) throws Exception {
+        if (!clients.containsKey(userId)) {
+            throw new Exception();
+        }
+        return clients.get(userId);
+    }
+}
