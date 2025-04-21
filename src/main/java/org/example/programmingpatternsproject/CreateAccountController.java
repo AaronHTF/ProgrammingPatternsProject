f0d673f8b60be5cef232db60ce27fc09fc17b638
Aaron <ajhotimfat@gmail.com>
2025-04-21 15:05:16 -0400

    Created the link between the create account window and the login window.

--- src/main/java/org/example/programmingpatternsproject/CreateAccountController.java
+++ src/main/java/org/example/programmingpatternsproject/CreateAccountController.java
@@ -1,4 +1,17 @@
 package org.example.programmingpatternsproject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
 public class CreateAccountController {
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    public void handleCloseButtonAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
 }
--- src/main/java/org/example/programmingpatternsproject/LoginController.java
+++ src/main/java/org/example/programmingpatternsproject/LoginController.java
@@ -1,4 +1,32 @@
 package org.example.programmingpatternsproject;
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @FXML
+    public Button createAccountButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void handleCreateAccountButtonAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createAccount.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Create Account");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
