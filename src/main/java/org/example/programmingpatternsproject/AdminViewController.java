7a31738eb2e91defdf29261afcec6885349f3efe
Aaron <ajhotimfat@gmail.com>
2025-04-28 13:41:10 -0400

    Designed the admin view and added the controls in the fxml file.

--- src/main/java/org/example/programmingpatternsproject/AdminViewController.java
+++ src/main/java/org/example/programmingpatternsproject/AdminViewController.java
@@ -1,4 +1,44 @@
 package org.example.programmingpatternsproject;
 
+import javafx.fxml.FXML;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 public class AdminViewController {
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private TableView<Ticket> ticketsTable;
+    @FXML
+    private TableColumn<Ticket, Integer> ticketIdColumn;
+    @FXML
+    private TableColumn<Ticket, String> clientIdColumn;
+    @FXML
+    private TableColumn<Ticket, String> flightIdColumn;
+    @FXML
+    private TableColumn<Ticket, String> dateColumn;
+    @FXML
+    private TableColumn<Ticket, String> classOfServiceColumn;
+    @FXML
+    private TableColumn<Ticket, String> statusColumn;
+    @FXML
+    private ChoiceBox<String> sourceChoiceBox;
+    @FXML
+    private ChoiceBox<String> destinationChoiceBox;
+    @FXML
+    private ChoiceBox<String> statusChoiceBox;
+    @FXML
+    private ChoiceBox<String> classChoiceBox;
 }
